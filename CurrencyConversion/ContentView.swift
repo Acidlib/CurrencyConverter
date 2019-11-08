a35e2ab3bcc24fb6db0f8688867b4fac00442050
Reetalibrarian <j51038@gmail.com>
2019-11-08 11:42:35 +0900

    Initial Commit

---
+++ CurrencyConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyConversion
+//
+//  Created by Yi-Ling Wu on 2019/11/08.
+//  Copyright © 2019 Yi-Ling Wu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
