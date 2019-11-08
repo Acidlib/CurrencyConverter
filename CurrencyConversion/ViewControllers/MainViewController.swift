c632e38791c4b0756866262b656e83d8e3cdb06b
Reetalibrarian <j51038@gmail.com>
2019-11-08 13:04:35 +0900

    [Structure] draft for viewControllers (add)

---
+++ CurrencyConversion/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  CurrencyConversion
+//
+//  Created by Yi-Ling Wu on 2019/11/08.
+//  Copyright © 2019 Yi-Ling Wu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ CurrencyConversion/ViewControllers/PortalViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PortalViewController.swift
+//  CurrencyConversion
+//
+//  Created by Yi-Ling Wu on 2019/11/08.
+//  Copyright © 2019 Yi-Ling Wu. All rights reserved.
+//
+
+import UIKit
+
+class PortalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
