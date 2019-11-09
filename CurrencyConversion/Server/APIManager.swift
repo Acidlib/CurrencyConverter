22f90d6378101c9727f51b3434389cb15d5b7fb0
Reetalibrarian <j51038@gmail.com>
2019-11-09 14:10:53 +0900

    [UI + coreData] images view
    * update image files
    * coreData handling (draft)

--- CurrencyConversion/Server/APIManager.swift
+++ CurrencyConversion/Server/APIManager.swift
@@ -13,9 +13,9 @@ let currencylayeraAiKey = "cc89cb9f60d8c11f89a042dd302cf584"
 
 class APIManager: APIManagerProtocol {
     static let shared = APIManager()
-    static var coreDataStoreName = "currencyHistory"
+    static var coreDataStoreName = "currencyDatabase"
     static var coreDataModel: NSManagedObjectModel = {
-        guard let modelURL = Bundle(for: APIManager.self).url(forResource: "currenyHistoryModel", withExtension: "momd"),
+        guard let modelURL = Bundle(for: APIManager.self).url(forResource: "CurrencyDatabase", withExtension: "momd"),
             let model = NSManagedObjectModel(contentsOf: modelURL) else {
                 fatalError("Can not init currenyHistoryModel CoreData")
         }
@@ -24,6 +24,7 @@ class APIManager: APIManagerProtocol {
     
     let container: NSPersistentContainer
     var context: NSManagedObjectContext
+    public var selectedArray: [String] = []
     
     init() {
         container = NSPersistentContainer(name: APIManager.coreDataStoreName, managedObjectModel: APIManager.coreDataModel)
@@ -46,6 +47,32 @@ class APIManager: APIManagerProtocol {
             }
             task.resume()
         } catch {
+        }
+    }
+    
+    // Local Database
+    func loadLocalEntities() {
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "SelectedCurrencyList")
+        do {
+            let fetchedArray = try context.fetch(fetchRequest)
+            if fetchedArray.count == 0 {
+                _ = SelectedCurrencyListEntity("Euro,EUR", context: context)
+                _ = SelectedCurrencyListEntity("United State,USD", context: context)
+                _ = SelectedCurrencyListEntity("Japan Yen,JPY", context: context)
+                _ = SelectedCurrencyListEntity("Great Britain Pound,GBP", context: context)
+                context.performAndWait {
+                    do {
+                        try context.save()
+                    } catch let error {
+                        print("store Logs failed \(error.localizedDescription)")
+                    }
+                }
+            }
+            selectedArray = fetchedArray.map({ //() -> String in
+                return $0.currencyNameAbbr
+            })
+        } catch let error {
+            print("fetch failed:\(error), \(error.localizedDescription)")
         }
     }
 }
